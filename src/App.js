15b447a48fa038da4da75659f6e1e41da18f7f53
Raneem <aljamal.raneem@gmail.com>
2022-12-20 12:41:02 +0200

    Weather API: Extracting the values from the Fake Weather json

--- src/App.js
+++ src/App.js
@@ -4,54 +4,68 @@ import WeatherNow from './Components/WeatherNow';
 import Mostlycloudy from "./images/weather-icons/mostlycloudy.svg";
 import Sunny from "./images/weather-icons/clear.svg";
 import Weather24 from './Components/Weather24';
+import FakeWeather from "./data/FakeWeather.json";
 
 function App() {
-  const weather = [
-    {
-      hour:'03:00',
-      image: Mostlycloudy,
-      temperature:'8\u00b0 C'
-    },
-    {
-      hour:'06:00',
-      image: Mostlycloudy,
-      temperature:'9\u00b0 C'
-    },
-    {
-      hour:'09:00',
-      image: Sunny,
-      temperature:'14\u00b0 C'
-    },
-    {
-      hour:'12:00',
-      image: Sunny,
-      temperature:'17\u00b0 C'
-    },
-    {
-      hour:'15:00',
-      image: Sunny,
-      temperature:'18\u00b0 C'
-    },
-    {
-      hour:'18:00',
-      image: Sunny,
-      temperature:'16\u00b0 C'
-    },
-    {
-      hour:'21:00',
-      image: Mostlycloudy,
-      temperature:'13\u00b0 C'
-    }
-  ]
+  // const weather = [
+  //   {
+  //     hour:'03:00',
+  //     image: Mostlycloudy,
+  //     temperature:'8\u00b0 C'
+  //   },
+  //   {
+  //     hour:'06:00',
+  //     image: Mostlycloudy,
+  //     temperature:'9\u00b0 C'
+  //   },
+  //   {
+  //     hour:'09:00',
+  //     image: Sunny,
+  //     temperature:'14\u00b0 C'
+  //   },
+  //   {
+  //     hour:'12:00',
+  //     image: Sunny,
+  //     temperature:'17\u00b0 C'
+  //   },
+  //   {
+  //     hour:'15:00',
+  //     image: Sunny,
+  //     temperature:'18\u00b0 C'
+  //   },
+  //   {
+  //     hour:'18:00',
+  //     image: Sunny,
+  //     temperature:'16\u00b0 C'
+  //   },
+  //   {
+  //     hour:'21:00',
+  //     image: Mostlycloudy,
+  //     temperature:'13\u00b0 C'
+  //   }
+  // ]
   return (
       <div className='wrapper'>
         <Search/>
         <WeatherNow/>
-        <div className='weather24'>
+        <div className='Weather24'>
+           {
+            FakeWeather.list.map( (item, index) => {
+              return(
+                <div className="box">
+                      <Weather24 weather={item.weather[0].id} icon={item.weather[0].icon}/>
+                </div>
+              )
+            })
+           }
+
+
+        </div>
+        {/* <div className='weather24'>
         {weather.map((item, index)=>(
         <Weather24 hour={item.hour} image={item.image} temperature={item.temperature}/>
         ))}
-        </div>
+        </div> */}
       </div>
   )
 }
--- src/Components/Weather24.js
+++ src/Components/Weather24.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import '../App.css'
 
-const Weather24 = ({hour, temperature, image}) => {
-    return (
-             <div className='weather'>
-                <p>{hour}</p>
-                <img src={image} className='image1'/>
-                <p>{temperature}</p>
-            </div>
+// const Weather24 = ({hour, temperature, image}) => {
+//     return (
+//              <div className='weather'>
+//                 <p>{hour}</p>
+//                 <img src={image} className='image1'/>
+//                 <p>{temperature}</p>
+//             </div>
    
-  )
-}
+//   )
+// }
 
+const Weather24 = ({weather, icon}) => {
+      return (
+               <div className='weather'>
+                  <p>{weather}</p>
+                  <img src={`http://openweathermap.org/img/wn/${icon}.png`} />
+                 
+              </div>
+     
+    )
+  } 
 export default Weather24
